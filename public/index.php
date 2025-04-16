a5b2f2d328f5d47a64c6d6b5f6c64f11722f0fbe
Elton Storari <eltonstorari@gmail.com>
2025-04-15 22:57:49 -0300

    Como que funciona o Out Buffer Work

--- public/index.php
+++ public/index.php
@@ -2,4 +2,16 @@
 
 namespace App;
 
-echo "Hello World!";
+$data = ['name' => 'Elton Storari'];
+ob_start();
+
+extract($data);
+
+require 'home.php';
+
+
+$content = ob_get_contents();
+
+ob_end_clean();
+
+var_dump($content); 
